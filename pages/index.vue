44f5dabddbaf2aa3ecd4f3f0f28b414a69307ad3
Kristaps Fabians Geikins <fabis94@live.com>
2023-09-18 14:30:44 +0300

    init commit & repro

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="index">
+        <NuxtLink to="/other" class="card">
+      <span>NuxtLink top text</span>
+      <NuxtLink @click.stop.prevent="onNestedClick">Nested nuxt link</NuxtLink>
+      <span>NuxtLink bottom text</span>
+    </NuxtLink>
+    </div>
+  </template>
+  <script setup lang="ts">
+  const onNestedClick = () => {
+    console.log("nested nuxtlink clicked")
+  }
+  </script>
+  <style>
+  .card {
+    display: flex;
+    flex-direction: column;
+    border: 1px solid black;
+    padding: 4px;
+    margin-left: auto;
+    margin-right: auto;
+    width: 300px;
+  }
+  </style>
+  
---
+++ pages/other.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="other">
+        <NuxtLink to="/">Go home</NuxtLink>
+    </div>
+</template>
